2e654e672f33f50cbb9cb2c22b916be3585fcb74
谢慧渝 <1210459691@qq.com>
2024-06-24 09:33:50 +0800

    初始化项目并配置路由

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Layout from '@/pages/Layout';
+import Month from '@/pages/Month';
+import Year from '@/pages/Year';
+import New from '@/pages/new';
+import { createBrowserRouter } from 'react-router-dom';
+
+export default createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: 'year',
+        element: <Year />
+      },
+      {
+        index: true,
+        // path: 'month',
+        element: <Month />
+      },
+    ]
+  },
+  {
+    path: 'new',
+    element: <New />
+  }
+])
